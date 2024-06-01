1fe9c269c3b60ff9460971290ee66a03e25b6826
Milomir Babic <redant333@gmail.com>
2024-06-01 16:36:38 +0200

    Remove lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
+mod button;
+pub use button::Button;
+
+mod pieces;
+pub use pieces::collection::Manipulation;
+pub use pieces::collection::PieceCollection;
+pub use pieces::path::Path;
+pub use pieces::path::SubpathNoId;
+pub use pieces::piece::Piece;
+pub use pieces::position::PieceError;
+pub use pieces::position::Position;
+
+pub mod game;
+pub mod initialization;
+pub mod solved_marker;
+
 use macroquad::prelude::*;
-use rubicross::game::Game;
-use rubicross::initialization::{
+use game::Game;
+use initialization::{
     initialize_buttons, initialize_paths, initialize_pieces, initialize_solved_markers, load_assets,
 };
 
